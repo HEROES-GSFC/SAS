0e6963eef0467174ef4c76458dfd1a8eee72cfc4
Steven Christe <steven.d.christe@nasa.gov>
2013-02-03 13:09:56 -0500

    setup threads for forwarder2, looks like forwarder1 and forwarder2 can be the same program

--- PYAS-core/forwarder2/forwarder2.c
+++ PYAS-core/forwarder2/forwarder2.c
@@ -4,66 +4,141 @@
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
+#include <pthread.h>    /* for multithreading */
 
 #define ECHOMAX 1024     /* Longest string to echo */
 
-/* This program listens for packet on UDP port 5000 and forwards them to forwardingIP */
-
-void DieWithError(char *errorMessage)
+void printWithError(char *errorMessage)
 {
     printf("%s", errorMessage);
 }
 
-int main()
+void *listenandForward1(void *arg)
 {
-    int sock;                        /* Socket */
-    struct sockaddr_in myAddr; /* Local address */
-    struct sockaddr_in senderAddr; /* Sender address */
-    struct sockaddr_in forwardingAddr; /* Forwarding address */
+    // This thread listens on port 5000 for packets from SAS 2
+    // and forwards them to the correct place
+    int sock;                           /* Socket */
+    struct sockaddr_in myAddr;          /* Local address */
+    struct sockaddr_in senderAddr;      /* Sender address */
+    struct sockaddr_in forwarding1Addr; /* Forwarding address 1 (myself on port 5010)*/
+    struct sockaddr_in forwarding2Addr; /* Forwarding address 1 (other IP on port 5000)*/
     
-    unsigned int cliAddrLen;         /* Length of incoming message */
-    char echoBuffer[ECHOMAX];        /* Buffer for echo string */
-    unsigned short listeningPort;     /* Server port */
-    int recvMsgSize;                 /* Size of received message */
+    unsigned int cliAddrLen;            /* Length of incoming message */
+    char echoBuffer[ECHOMAX];           /* Buffer for echo string */
+    unsigned short listeningPort;       /* The port to listen to */
+    unsigned short forwardingPort;     /* port 1 to forward to */
+    int recvMsgSize;                    /* Size of received message */
 
     // listen to port 5000 for UDP packets and forward them to 192.168.2.2
-    char *forwadingIP = "192.168.2.2";     /* IP address to forward to */
-	listeningPort = 5000;
+    // char *forwadingIP = "192.168.2.1"; /* IP address to forward to */
+        
+    listeningPort = 5000;
 
-    /* Create socket for sending/receiving datagrams */
+    /* Create socket for receiving datagrams */
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-        DieWithError("socket() failed");
+        printWithError("socket() failed");
 
     /* Construct local address structure */
-    memset(&myAddr, 0, sizeof(myAddr));   /* Zero out structure */
+    memset(&myAddr, 0, sizeof(myAddr));         /* Zero out structure */
     myAddr.sin_family = AF_INET;                /* Internet address family */
     myAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-    myAddr.sin_port = htons(listeningPort);      /* Local port */
+    myAddr.sin_port = htons(listeningPort);     /* Local port */
 
-    /* Construct the forwarding address structure */
-    memset(&forwardingAddr, 0, sizeof(myAddr));    /* Zero out structure */
-    forwardingAddr.sin_family = AF_INET;                 /* Internet addr family */
-    forwardingAddr.sin_addr.s_addr = inet_addr(forwadingIP);  /* Server IP address */
-    forwardingAddr.sin_port   = htons(listeningPort);     /* Port to forward on is same as listening port*/
+   /* Construct one forwarding address structure */
+    //memset(&forwarding1Addr, 0, sizeof(myAddr));    /* Zero out structure */
+    //forwarding1Addr.sin_family = AF_INET;                 /* Internet addr family */
+    //forwarding1Addr.sin_addr.s_addr = inet_addr(forwadingIP1);  /* Server IP address */
+    //forwarding1Addr.sin_port   = htons(forwardingPort1);     /* Port to forward on is same as listening port*/
 
     /* Bind to the local address */
     if (bind(sock, (struct sockaddr *) &myAddr, sizeof(myAddr)) < 0)
-        DieWithError("bind() failed");
+        printWithError("bind() failed");
   
-    for (;;) /* Run forever */
-    {
-        /* Set the size of the in-out parameter */
+	while(1)    // run forever
+	{
+         /* Set the size of the in-out parameter */
         cliAddrLen = sizeof(senderAddr);
-
+    
         /* Block until receive message from a client */
         if ((recvMsgSize = recvfrom(sock, echoBuffer, sizeof(echoBuffer), 0,
             (struct sockaddr *) &senderAddr, &cliAddrLen)) < 0)
-            DieWithError("recvfrom() failed");
+            printWithError("recvfrom() failed");
+        printf("received packet on port %i from %s\n", listeningPort, inet_ntoa(senderAddr.sin_addr));
+	}
 
-        /* Forward UDP packet */
-        if (sendto(sock, echoBuffer, recvMsgSize, 0, 
-             (struct sockaddr *) &forwardingAddr, sizeof(forwardingAddr)) != recvMsgSize)
-            DieWithError("sendto() sent a different number of bytes than expected");
-    }
-    /* NOT REACHED */
+    /* NEVER REACHED */
+	return NULL;
 }
+
+void *listenandForward2(void *arg)
+{
+    // and forwards them to the correct place
+    int sock;                           /* Socket */
+    struct sockaddr_in myAddr;          /* Local address */
+    struct sockaddr_in senderAddr;      /* Sender address */
+    struct sockaddr_in forwarding1Addr; /* Forwarding address 1 (myself on port 5010)*/
+    struct sockaddr_in forwarding2Addr; /* Forwarding address 1 (other IP on port 5000)*/
+    
+    unsigned int cliAddrLen;            /* Length of incoming message */
+    char echoBuffer[ECHOMAX];           /* Buffer for echo string */
+    unsigned short listeningPort;       /* The port to listen to */
+    unsigned short forwardingPort;     /* port 1 to forward to */
+    int recvMsgSize;                    /* Size of received message */
+
+    // listen to port 5000 for UDP packets and forward them to 192.168.2.2
+    // char *forwadingIP = "192.168.2.1"; /* IP address to forward to */
+        
+    listeningPort = 5002;
+
+    /* Create socket for receiving datagrams */
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+        printWithError("socket() failed");
+
+    /* Construct local address structure */
+    memset(&myAddr, 0, sizeof(myAddr));         /* Zero out structure */
+    myAddr.sin_family = AF_INET;                /* Internet address family */
+    myAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    myAddr.sin_port = htons(listeningPort);     /* Local port */
+
+   /* Construct one forwarding address structure */
+    //memset(&forwarding1Addr, 0, sizeof(myAddr));    /* Zero out structure */
+    //forwarding1Addr.sin_family = AF_INET;                 /* Internet addr family */
+    //forwarding1Addr.sin_addr.s_addr = inet_addr(forwadingIP1);  /* Server IP address */
+    //forwarding1Addr.sin_port   = htons(forwardingPort1);     /* Port to forward on is same as listening port*/
+
+    /* Bind to the local address */
+    if (bind(sock, (struct sockaddr *) &myAddr, sizeof(myAddr)) < 0)
+        printWithError("bind() failed");
+  
+	while(1)    // run forever
+	{
+         /* Set the size of the in-out parameter */
+        cliAddrLen = sizeof(senderAddr);
+    
+        /* Block until receive message from a client */
+        if ((recvMsgSize = recvfrom(sock, echoBuffer, sizeof(echoBuffer), 0,
+            (struct sockaddr *) &senderAddr, &cliAddrLen)) < 0)
+            printWithError("recvfrom() failed");
+        printf("received packet on port %i from %s\n", listeningPort, inet_ntoa(senderAddr.sin_addr));
+	}
+
+    /* NEVER REACHED */
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t pth1;	// this is our thread identifier
+	pthread_t pth2;	// this is our thread identifier
+
+	/* Create worker threads */
+	
+	pthread_create(&pth1,NULL,listenandForward1,"processing...");   // listen on port 5000
+	pthread_create(&pth2,NULL,listenandForward2,"processing...");   // listen on port 5002
+
+	while(1){} /* never stop */
+
+    /* NEVER REACHED */
+	return 0;
+}
+
