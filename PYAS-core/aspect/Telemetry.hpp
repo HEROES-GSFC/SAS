fdc63b69298b44ab3d82a02a566f5af9105164f0
Albert Y. Shih <ayshih@gmail.com>
2013-02-09 13:39:54 -0500

    TelemetryPacketQueue class

--- PYAS-core/aspect/Telemetry.hpp
+++ PYAS-core/aspect/Telemetry.hpp
@@ -1,5 +1,8 @@
 /*
 
+TelemetryPacket and TelemetryPacketQueue
+  derived from Packet and std::list<TelemetryPacket>
+
 //Telemetry packet from SAS containing an array
 uint8_t image[5] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
 TelemetryPacket tp2(0x70, 0x30);
@@ -11,6 +14,9 @@ std::cout << tp2 << std::endl;
 
 #ifndef _TELEMETRY_HPP_
 #define _TELEMETRY_HPP_
+
+#include <list>
+#include <iostream>
 
 #include "Packet.hpp"
 
@@ -38,6 +44,25 @@ class TelemetryPacket : public Packet {
 
     uint8_t getTypeID();
     uint8_t getSourceID();
+};
+
+class TelemetryPacketQueue : public std::list<TelemetryPacket> {
+
+  public:
+    TelemetryPacketQueue() {};
+
+  //insertion operator <<
+  //Overloaded to add TelemetryPacket objects
+  //  or TelemetryPacket objects from a TelemetryPacketQueue
+  //In the latter case, the source TelemetryPacketQueue is emptied
+  friend TelemetryPacketQueue &operator<<(TelemetryPacketQueue &tpq, const TelemetryPacket &tp);
+  friend TelemetryPacketQueue &operator<<(TelemetryPacketQueue &tpq, TelemetryPacketQueue &other);
+
+  //insertion operator << for ostream output
+  friend std::ostream &operator<<(std::ostream &os, TelemetryPacketQueue &tpq);
+
+  //extraction operator >> for popping off the next TelemetryPacket object
+  friend TelemetryPacketQueue &operator>>(TelemetryPacketQueue &tpq, TelemetryPacket &tp);
 };
 
 #endif
