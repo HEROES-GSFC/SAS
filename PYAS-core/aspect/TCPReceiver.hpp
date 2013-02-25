d947d8be7dd9dac9b87ea00e445dce4dd38eb7e6
Steven Christe <steven.d.christe@nasa.gov>
2013-02-24 23:34:53 -0500

    fix to constructor in tcpreceiver

--- PYAS-core/aspect/TCPReceiver.hpp
+++ PYAS-core/aspect/TCPReceiver.hpp
@@ -6,35 +6,27 @@
 #include "Command.hpp"
 #include "Telemetry.hpp"
 
+#define PACKET_MAX_SIZE 300
+
 class TCPReceiver {
     protected:
-        int sock;                       /* Socket */
+        int sender_sock;                /* Socket of someone connecting to me */
+        int my_sock;                /* My Socket */
         struct sockaddr_in myAddr;      /* Local address */
         struct sockaddr_in senderAddr;  /* Sender address */
     
-        unsigned int cliAddrLen;        /* Length of incoming message */
+        unsigned int senderAddrLen;        /* Length of incoming message */
         char payload[PACKET_MAX_SIZE];  /* Buffer for echo string */
         unsigned short listeningPort;   /* The port to listen to */
-        int recvMsgSize;                /* Size of received message */
+        int numBytesRcvd;                /* Size of received message */
 
     public:
-        UDPReceiver( void );
-        UDPReceiver( unsigned short port );
+        TCPReceiver( void );
+        TCPReceiver( unsigned short port );
         
         unsigned int listen( void );
         void get_packet( uint8_t *packet  );
+        void handle_tcpclient( int client_socket )
         void init_connection( void );
         void close_connection( void );
-};
-
-class CommandReceiver: public UDPReceiver {
-
-    public:
-        CommandReceiver( unsigned short port );
-};
-
-class TelemetryReceiver: public UDPReceiver {
-
-    public:
-        TelemetryReceiver( unsigned short port );
 };
