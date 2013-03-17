247705dcc09fc52032535007d8e65236a1a37128
Albert Y. Shih <ayshih@gmail.com>
2013-03-17 01:14:34 -0400

    Pair and Pair3B classes

---
+++ PYAS-core/aspect/types.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <math.h>
+
+#include "types.hpp"
+
+using std::ostream;
+
+Pair::Pair(float x, float y) : i_x(x), i_y(y) {}
+Pair::Pair(const Pair3B& p3) : i_x(p3.x()), i_y(p3.y()) {}
+
+ByteString& operator<<(ByteString& bs, const Pair& p)
+{
+  return bs << p.x() << p.y();
+}
+
+ostream& operator<<(ostream& os, const Pair& p)
+{
+  return os << "(" << p.x() << ", " << p.y() << ")";
+}
+
+ByteString& operator>>(ByteString& bs, Pair& p)
+{
+  return bs >> p.i_x >> p.i_y;
+}
+
+Pair3B::Pair3B(float x, float y)
+{
+  initialize(x, y);
+}
+
+Pair3B::Pair3B(const Pair& p)
+{
+  initialize(p.x(), p.y());
+}
+
+void Pair3B::initialize(float x, float y)
+{
+  //Needs bounds checking!
+  i_a = (uint16_t)floor((x*3+1)+0.5);
+  i_b = (uint16_t)floor((y*3+1)+0.5);
+}
+
+float Pair3B::x() const { return ((float)i_a-1)/3; }
+float Pair3B::y() const { return ((float)i_b-1)/3; }
+
+ByteString& operator<<(ByteString& bs, const Pair3B& p3)
+{
+  uint8_t buffer[3];
+  buffer[0] = (uint8_t)((p3.i_a & 0x0ff0) >> 4);
+  buffer[1] = (uint8_t)(((p3.i_a & 0x000f) << 4) | ((p3.i_b & 0x0f00) >> 8));
+  buffer[2] = (uint8_t)(p3.i_b & 0x00ff);
+  bs.append_bytes(buffer, 3);
+  return bs;
+}
+
+ostream& operator<<(ostream& os, const Pair3B& p3)
+{
+  return os << "(" << p3.x() << ", " << p3.y() << ")";
+}
+
+ByteString& operator>>(ByteString& bs, Pair3B& p3)
+{
+  uint8_t buffer[3];
+  bs.readNextTo_bytes(buffer, 3);
+  p3.i_a = (buffer[0] << 4) | ((buffer[1] & 0xf0) >> 4);
+  p3.i_b = ((buffer[1] & 0x0f) << 8) | buffer[2];
+  return bs;
+}
+
---
+++ PYAS-core/aspect/types.hpp
@@ -0,0 +1,77 @@
+/*
+
+Pair, Pair3B
+
+These classes are for pairs of floats.  Pair is not particularly more useful
+than other alternatives, but Pair3B is critical for writing reduced-precision
+pixel location floats to ByteStrings.  Two 2-byte floats are stored in only
+3 bytes by rounding to the nearest third of a pixel.
+
+For now, the actual values are kept private, and can be read by using x() and
+y().  The only way to set the values after the construction is using the
+ByteString insertion operator.
+
+To write a Pair3B:
+  Pair3B a(123.45,56.78);
+  ByteString bs;
+  bs << a;
+
+To write a Pair as a Pair3B:
+  Pair p(135.79,246.80);
+  bs << Pair3B(p);
+
+To read a Pair3B:
+  Pair3B b;
+  bs >> b;
+
+*/
+
+
+#ifndef _TYPES_HPP_
+#define _TYPES_HPP_
+
+#include <iostream>
+#include <stdint.h>
+
+#include "Packet.hpp"
+
+class Pair;
+class Pair3B;
+
+class Pair {
+  private:
+    float i_x, i_y;
+
+  public:
+    Pair(float x = 0, float y = 0);
+    Pair(const Pair3B& p3);
+
+    float x() const { return i_x; };
+    float y() const { return i_y; };
+
+    friend ByteString& operator<<(ByteString& bs, const Pair& p);
+    friend std::ostream& operator<<(std::ostream& os, const Pair& p);
+
+    friend ByteString& operator>>(ByteString& bs, Pair& p);
+};
+
+class Pair3B {
+  private:
+    uint16_t i_a, i_b;
+
+    void initialize(float x, float y);
+
+  public:
+    Pair3B(float x = 0, float y = 0);
+    Pair3B(const Pair& p);
+
+    float x() const;
+    float y() const;
+
+    friend ByteString& operator<<(ByteString& bs, const Pair3B& p3);
+    friend std::ostream& operator<<(std::ostream& os, const Pair3B& p3);
+
+    friend ByteString& operator>>(ByteString& bs, Pair3B& p3);
+};
+
+#endif
