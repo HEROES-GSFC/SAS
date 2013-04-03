ab9405cf974155f5b048fc314a60d2cf70e2cd37
Alex Cramer <akcramer@gmail.com>
2013-04-03 13:03:19 -0400

    Programs for generating videos of SBC behavior (AspectVideo) and looking at aspect module behavior (AspectTest)

--- PYAS-core/aspect/utilities.hpp
+++ PYAS-core/aspect/utilities.hpp
@@ -1,4 +1,5 @@
 #pragma once 
+#include <opencv.hpp>
 #include <pthread.h>
 #include <chrono>
 #include <string>
@@ -32,3 +33,5 @@ private:
 
 timespec TimespecDiff(timespec start, timespec end);
 const std::string nanoString(long tv_nsec);
+void DrawCross(cv::Mat &image, cv::Point2f point, cv::Scalar color, int length, int thickness, int resolution);
+
