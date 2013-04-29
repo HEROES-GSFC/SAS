808100d2e7b62030e20b3adf53f188d8f71b260c
Albert Shih <ayshih@gmail.com>
2013-04-29 17:41:03 -0500

    Properly added SMBus files and updated installation scripts

--- os/install-sas1.sh
+++ os/install-sas1.sh
@@ -12,6 +12,7 @@ sudo cp 10-network-sas${sas_id}.rules /etc/udev/rules.d/10-network.rules
 echo "Copying configuration files..."
 sudo cp sysctl.conf /etc/sysctl.conf
 sudo cp etc-conf.d-network /etc/conf.d/network
+sudo cp etc-conf.d-lm_sensors /etc/conf.d/lm_sensors
 
 echo "Setting up network-configuration service..."
 sudo cp network-sas${sas_id}.service /etc/systemd/system/network.service
