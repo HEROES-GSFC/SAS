f015105af6104003ebed1dfdb83f776cf621601f
Albert Y. Shih <ayshih@gmail.com>
2013-05-11 17:45:35 -0500

    More documentation for evaluate

--- PYAS-core/aspect/evaluate.cpp
+++ PYAS-core/aspect/evaluate.cpp
@@ -5,14 +5,19 @@ Pass in a list of FITS files on the command line, e.g.:
     ./evaluate *.fits
     ./evaluate `ls /mnt/disk2/*.fits`
 
-What's reported is the filename, the image min, the image max, and a histogram
-
+What's reported is the filename, the image min, the image max, and a histogram:
+-----
+/mnt/disk2/pyas_130511_180030_000001.fits   0 210 c7655555555555566666777875200000
+-----
 The histogram uses 32 bins (so 8 values per bin), and the value in each bin is
 represented by 2*log10(value+1), rounded, in hex (tops out at "c").
 Thus, "0" really means 0 pixels,
       "1" means 1 to 4 pixels,
       "2" means 5 to 16 pixels,
       "3" means 17 to 55 pixels, and so on
+Note that a "good" PYAS histogram can be recognized by being mostly flat for
+the lower half of the histogram and then peaking in the upper half of the
+histogram
 */
 
 #include <cstdio>
