2589fc132439e712eebe653e3350e4cc0cec4d3c
Alex Cramer <akcramer@gmail.com>
2013-07-19 23:26:16 -0400

    relays.cpp compiles and runs on SBC, but only flips a single relay

--- aspect/relays.cpp
+++ aspect/relays.cpp
@@ -9,10 +9,17 @@ int main(int argc, char* argv[])
     int relayID;
     bool value;
     for (int k = 0; k < 16; k++)
-    {
+    	{
         relayID = k;
+
         relays.getRelay(relayID, value);
         std::cout << "Relay " << relayID << " is ";
-        std::cout << ((value) ? "ON" : "OFF") << std::endl;
-    }
+        std::cout << value << std::endl;
+
+	relays.setRelay(relayID, 0);
+
+	relays.getRelay(relayID, value);
+	std::cout << "Relay " << relayID << " is now ";
+	std::cout << value << std::endl;
+}
 }
