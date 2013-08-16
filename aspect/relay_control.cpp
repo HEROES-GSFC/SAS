76ee014661f8740b1420254ef78c0f1291b1ea7d
Albert Y. Shih <ayshih@gmail.com>
2013-08-16 17:42:30 -0600

    First attempt at the relay-control service

---
+++ aspect/relay_control.cpp
@@ -0,0 +1,47 @@
+/*
+Service to handle relay control
+
+Currently no ability to report relay bank
+e.g., relays.getRelay(i, state);
+*/
+
+#define PORT_RELAY_CONTROL 4567
+
+#define PASSPHRASE "tAzh0Sh?$:dGo4t8j$8ceh^,d;2#ob}j_VEHXtWrI_AL*5C3l/edTMoO2Q8FY&K"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "UDPReceiver.hpp"
+#include "pmm/DiamondPMM.h"
+
+int main()
+{
+    UDPReceiver receiver(PORT_RELAY_CONTROL);
+    receiver.init_connection();
+
+    DiamondPMM relayBoard;
+    DiamondPMMStateRelay relays(relayBoard);
+
+    uint16_t packet_length;
+    uint8_t *array;
+
+    while(1)
+    {
+        packet_length = receiver.listen();
+        array = new uint8_t[packet_length];
+        receiver.get_packet(array);
+
+        if((packet_length == strlen(PASSPHRASE))+2 && (strncmp((char *)array, PASSPHRASE, strlen(PASSPHRASE)) == 0)) {
+            uint8_t relay_number = array[packet_length-2];
+            bool on_if_true = array[packet_length-1];
+            printf("Valid relay-control passphrase received: %d %s\n", relay_number, (on_if_true ? "ON" : "OFF"));
+            relays.setPort(on_if_true, relay_number);
+        } else {
+            printf("Invalid relay-control passphrase received: %s\n", array);
+        }
+        delete array;
+    }
+
+    return 0;
+}
