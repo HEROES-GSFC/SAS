b28b214b89cde7bb066561ac74236eec690c49ee
Albert Y. Shih <ayshih@gmail.com>
2013-08-17 17:03:15 -0600

    Transform class can now accept non-now times as input

--- aspect/Transform.hpp
+++ aspect/Transform.hpp
@@ -5,12 +5,20 @@
   Azimuth is defined as eastward from North
   Elevation is defined as poleward from horizontal
 
-
+  To specify a particular time, some methods accept a tm struct from time.h
+  It has members:
+    tm_year (year minus 1900)
+    tm_mon (month minus 1)
+    tm_mday (1-31)
+    tm_hour (specify in UT)
+    tm_min
+    tm_sec
 */
 
 #ifndef _TRANSFORM_HPP_
 #define _TRANSFORM_HPP_
 
+#include <time.h>
 #include <iostream>
 
 #include "types.hpp"
@@ -53,26 +61,33 @@ private:
     double elevation, elevation2;
     double orientation; //Calculated apparent orientation of the Sun's north pole, measured eastward from zenith
 
-    void prep();
-
-public:
-    Transform(Location location = HUNTSVILLE, Environment environment = GROUND);
-
-    Pair getSunAzEl(); //azimuth/elevation of the Sun
-    Pair getTargetAzEl(); //azimuth/elevation of the solar target
-    Pair getPointingAzEl(const Pair& sunPixel); //azimuth/elevation of the current pointing
-
+    //Converts detector pixel coordinates to an angular shift
+    //Output format is that used by translateAzEl()
     Pair getAngularShift(const Pair& sunPixel);
 
-    //Be careful, this initiates a *new* calulation
-    double getOrientation();
-    double getOrientationWithoutRecalculation() const;
-
-    //returns new azimuth/elevation
+    //Returns a new azimuth/elevation
+    //Shifts azel by angle amount.x() in direction amount.y()
+    //See code for more explanation
     Pair translateAzEl(const Pair& amount, const Pair& azel);
 
-    void report();
-    Pair calculateOffset(const Pair& sunPixel);
+public:
+    Transform(Location location = FORT_SUMNER,
+              Environment environment = GROUND);
+
+    //This function must be called sometime before any get* methods
+    void calculate(struct tm *input_time);
+
+    //These functions return the azimuth/elevation of points of interest
+    Pair getSunAzEl(); // Sun center
+    Pair getTargetAzEl(); // solar target
+    Pair getPointingAzEl(const Pair& sunPixel); // spot on screen in screen physical coordinates (mils)
+
+    double getOrientation() const;
+
+    //These three methods *do* call calculate() internally
+    void report(struct tm *input_time = NULL);
+    Pair calculateOffset(const Pair& sunPixel, struct tm *input_time = NULL);
+    double calculateOrientation(struct tm *input_time = NULL);
 
     void set_conversion(const Pair& intercept, const Pair& slope);
     void set_calibrated_center(const Pair& arg);
